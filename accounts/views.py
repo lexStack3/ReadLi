bea5aed27c63e524457578b93841e023a9d4e675
lexStack3 <alexanderedim80@gmail.com>
2026-02-24 23:13:23 +0100

    Created a function base signup_view, profile_view, and profile_update_view viewsets

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import SignupForm, ProfileUpdateForm
 
-# Create your views here.
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Account created successfully!")
+            return redirect ("profile")
+    else:
+        form = SignupForm()
+
+    return render(request, "accounts/signup.html", {"form": form})
+
+
+@login_required
+def profile_view(request):
+    return render(request, "accounts/profile.html", {"user": request.user})
+
+@login_required
+def profile_update_view(request):
+    if request.method == "POST":
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated successfully!")
+            return redirect("profile")
+    else:
+        form = ProfileUpdateForm(instance=request.user)
+
+    return render(request, "accounts/profile_edit.html", {"form": form})
