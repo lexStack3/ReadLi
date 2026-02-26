49710dff9ee0bdcfdcac9f35fdcfc4c0c31edc17
lexStack3 <alexanderedim80@gmail.com>
2026-02-26 05:50:56 +0100

    Registered /categories and /books endpoints into api router

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import UserViewSet, AuthorViewSet
+from .views import (
+    UserViewSet, AuthorViewSet,
+    CategoryViewSet, BookViewSet
+)
 
 
 router = routers.DefaultRouter()
 
 router.register(r'accounts', UserViewSet, basename='accounts')
 router.register(r'authors', AuthorViewSet, basename='authors')
+router.register(r'categories', CategoryViewSet, basename='categories')
+router.register(r'books', BookViewSet, basename='book')
 
 urlpatterns = [
     path('', include(router.urls)),
