c71ddb94e0bcb7455c5c3bb4739dd0162bc47c71
lexStack3 <alexanderedim80@gmail.com>
2026-02-26 05:14:43 +0100

    Registered /authors endpoint to router

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import UserViewSet
+from .views import UserViewSet, AuthorViewSet
 
 
 router = routers.DefaultRouter()
 
 router.register(r'accounts', UserViewSet, basename='accounts')
+router.register(r'authors', AuthorViewSet, basename='authors')
 
 urlpatterns = [
     path('', include(router.urls)),
