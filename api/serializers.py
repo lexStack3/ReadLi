59ca9741c2d55e11ecc43ae0f654ded40c0eafca
lexStack3 <alexanderedim80@gmail.com>
2026-02-26 05:49:40 +0100

    fate: Created model serializers for Category and Book models

--- api/serializers.py
+++ api/serializers.py
@@ -46,3 +46,64 @@ class AuthorSerializer(serializers.ModelSerializer):
             'author_id', 'name', 'biography', 'date_of_birth',
             'books', 'created_at', 'updated_at'
         ]
+
+
+class CategoryBooksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['book_id', 'title']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    """
+    A <Category> model serializer.
+    """
+    books = CategoryBooksSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = [
+            'category_id', 'name', 'description', 'books',
+            'created_at', 'updated_at'
+        ]
+
+
+class BookCategoriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['category_id', 'name']
+
+
+class BookAuthorsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['author_id', 'name']
+
+
+class BookSerializer(serializers.ModelSerializer):
+    """
+    A <Book> model serializer.
+    """
+    authors = BookAuthorsSerializer(many=True, read_only=True)
+    author_ids = serializers.PrimaryKeyRelatedField(
+        many=True,
+        write_only=True,
+        source='authors',
+        queryset=Author.objects.all()
+    )
+    categories = BookCategoriesSerializer(read_only=True, many=True)
+    category_ids = serializers.PrimaryKeyRelatedField(
+        many=True,
+        write_only=True,
+        source='categories',
+        queryset=Category.objects.all(),
+    )
+
+    class Meta:
+        model = Book
+        fields = [
+            'book_id', 'title', 'description', 'isbn',
+            'publication_date', 'total_copies', 'available_copies',
+            'author_ids', 'authors', 'categories', 'category_ids',
+            'created_at', 'updated_at'
+        ]
