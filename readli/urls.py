20dc5ec871435b6efcb2facf15e9f6dcdf92e894
lexStack3 <alexanderedim80@gmail.com>
2026-03-01 00:13:49 +0100

    Added home endpoint

--- readli/urls.py
+++ readli/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import index
 
 urlpatterns = [
+    path('', index, name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('api/', include('api.urls')),
