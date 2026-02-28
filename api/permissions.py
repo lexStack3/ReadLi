d13d1b535717523c1c330c947a2e6acf9f99aaf2
lexStack3 <alexanderedim80@gmail.com>
2026-02-28 23:19:11 +0100

    Fate: Created IsLibrarian and IsOwner permissions

---
+++ api/permissions.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+
+from rest_framework import permissions
+from library.models import BorrowRecord
+
+
+User = get_user_model()
+
+
+class IsOwner(permissions.BasePermission):
+    """
+    Grants access to the owner of an object.
+    """
+    def has_object_permission(self, request, view, obj):
+        if isinstance(obj, User):
+            return request.user == obj
+        elif isinstance(obj, BorrowBook):
+            return request.user == obj.user
+
+        return False
+
+
+class IsLibrarian(permissions.BasePermission):
+    """
+    Grants access to a librarian.
+    """
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated and
+            request.user.role == User.Role.LIBRARIAN
+        )
