adc8b330c7a207c79213006be8688ce5677f8ffe
lexStack3 <alexanderedim80@gmail.com>
2026-03-06 09:18:53 +0100

    Fate: fixed issue of listing books per authors and categories

--- api/views.py
+++ api/views.py
@@ -80,21 +80,12 @@ class AuthorViewSet(viewsets.ModelViewSet):
     filter_class = AuthorFilter
     search_fields = ['name']
 
-    def get_queryset(self):
-        queryset = Author.objects.prefetch_related('books')
-
-        # In case of /authors/{author_id}/books route
-        author_id = self.kwargs.get('author_id', None)
-        if author_id:
-            return queryset.filter(books__author__author_id=author_id)
-
-        return queryset
-
     def get_permissions(self):
         if self.action in ['create', 'update', 'partial_update', 'destroy']:
             return [(IsAdminUser | IsLibrarian)()]
 
         return super().get_permissions()
+
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -105,16 +96,6 @@ class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     filter_class = CategoryFilter
     search_fields = ['name']
-
-    def get_queryset(self):
-        queryset = Category.objects.prefetch_related('books')
-
-        # incase of /categories/{category_id}/books
-        category_id = self.kwargs.get('category_id', None)
-        if category_id:
-            queryset = queryset.filter(books__category_id=category_id)
-
-        return queryset
 
     def get_permissions(self):
         if self.action in ['create', 'update', 'partial_update', 'destroy']:
@@ -137,10 +118,15 @@ class BookViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = Book.objects.prefetch_related("categories", "authors")
 
-        # in case of /authors/{author_id}/books route
-        author_id = self.kwargs.get('author_id', None)
+        # In case of /authors/{author_id}/books route
+        author_id = self.kwargs.get('author_pk', None)
         if author_id:
             queryset = queryset.filter(authors__author_id=author_id)
+
+        # In case of /categories/{category_pk}/books route
+        category_id = self.kwargs.get('category_pk', None)
+        if category_id:
+            queryset = queryset.filter(categories__category_id=category_id)
 
         return queryset
 
