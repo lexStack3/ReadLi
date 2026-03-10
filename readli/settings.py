832ebf75631dc516c8455b9de8dcd6aab7138ced
lexStack3 <alexanderedim80@gmail.com>
2026-03-11 00:07:45 +0100

    Fate: Debug on static file configuration

--- readli/settings.py
+++ readli/settings.py
@@ -137,8 +137,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-APPEND_SLASH = False
-
 AUTH_USER_MODEL = 'accounts.User'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
@@ -149,8 +147,9 @@ LOGOUT_REDIRECT_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # REST_FRAMEWORK CONFIGURATIONS
 
