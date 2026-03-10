46796c5eff4f0af2deaaaaf5e698cb8a972e06cf
lexStack3 <alexanderedim80@gmail.com>
2026-03-11 00:07:08 +0100

    Fate: Frontend viewset

--- library/views.py
+++ library/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from library.models import Book, Author, Category, BorrowRecord
+
+
+class HomeView(ListView):
+    model = Book
+    template_name = "index.html"
+    context_object_name = "books"
+
+
+class BooksView(ListView):
+    model = Book
+    template_name = "books.html"
+    context_object_name = "books"
+
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = "book.html"
+    context_object_name = "book"
+
+
+class CategoriesView(ListView):
+    model = Category
+    template_name = "categories.html"
+    context_object_name = "categories"
+
+
+class AuthorsView(ListView):
+    model = Author
+    template_name = "authors.html"
+    context_object_name = "authors"
+
+
+class DashboardView(LoginRequiredMixin, ListView):
+    model = BorrowRecord
+    template_name = "dashboard.html"
+    context_object_name = "records"
+    login_url = "login"
+
+
+class LoginView(TemplateView):
+    template_name = "login.html"
+
+
+class SignupView(TemplateView):
+    template_name = "signup.html"
